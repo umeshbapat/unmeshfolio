33b5a6373c758efeca87c72ca1ae9463e2c9a718
Umesh Bapat <umeshbapat@quickenloans.com>
2021-01-19 21:39:03 -0500

    image changes and size updates

--- uva-app/src/app/app.component.ts
+++ uva-app/src/app/app.component.ts
@@ -1,10 +1,33 @@
 import { Component } from '@angular/core';
+import { ImageService } from './ImageService';
+
+export interface slider {
+  image: string,
+  thumbImage: string,
+  title: string
+};
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'uva-app';
+  imageObject: Array<slider> = [];
+
+  constructor(private heroService: ImageService) {
+    this.setImageObject();
+  }
+
+  setImageObject() {
+    this.heroService.getImages().subscribe((data: any) => {
+      this.imageObject = data;
+    });
+  }
+
+  imageClickHandler(e: any) {
+    console.log(e);
+  }
 }
--- uva-app/src/app/app.module.ts
+++ uva-app/src/app/app.module.ts
@@ -2,15 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { NgImageSliderModule } from 'ng-image-slider';
+import { ImageService } from './ImageService';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgImageSliderModule,
+    FormsModule, 
+    HttpClientModule
   ],
-  providers: [],
+  providers: [
+    ImageService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
